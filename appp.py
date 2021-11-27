a9a229ee70e0becd1da6e71870f8c45d878d6e3c
Palak1105 <123palakgupta@gmail.com>
2021-11-27 11:51:30 +0530

    few changes

--- appp.py
+++ appp.py
@@ -16,6 +16,7 @@ def load_data():
 
 st.sidebar.header("Project Options")
 
+
 options = ['About the project',
             'Types of Areas',
              'State vs so2',
@@ -62,16 +63,15 @@ elif choice == options[2]:
     st.header("Comparison of States VS SO2")
     color = st.sidebar.color_picker("select graph color")
     fig,ax = plt.subplots()
-    state_limit = st.sidebar.number_input("How many states",5,50,10)
-    fig,ax = plt.subplots()
-    df[['so2','state']].groupby(["state"]).median().sort_values(by='so2',ascending=False).head(state_limit).plot(
-                    kind='bar', 
-                    figsize=(15,10),
-                    ax=ax,
-                    color=color)
-    plt.title(" Comparison of states vs so2")
-    st.pyplot(fig)
-    st.info('From the above figure, we see that so2 level is highest in Uttranchal and lowest in Madhya Pradesh.')
+    state = st.sidebar.radio("select the state",df.state.unique().tolist())
+
+    try:
+       fig,ax=plt.subplots()
+       df[df['state']==state].set_index('so2')['Estimated level of so2 (%)'].plot(kind='bar',figsize=(5,15),ax=ax,color=color)
+       plt.title(f'so2 in {state}',fontsize="11",color='c')
+       st.pyplot(fig)
+    except Exception as e:
+        st.error(f"Please ask the admin {e}")
 
     
 
@@ -88,6 +88,7 @@ elif choice == options[3]:
                     color=color)
     plt.title(" Comparison of states vs no2")
     st.pyplot(fig)
+    st.info('From the above figure, we concluded that NO2 level is West Bengal ')
 
 elif choice == options[4]:
     st.header(" Comparison Of States VS RSPM")
@@ -96,12 +97,13 @@ elif choice == options[4]:
     state_limit = st.sidebar.number_input("How many states",3,25,10)
     fig,ax = plt.subplots()
     df[['rspm','state']].groupby(["state"]).median().sort_values(by='rspm',ascending=False).head(state_limit).plot(
-                   kind='line', 
+                   kind='bar', 
                     figsize=(15,10),
                     ax=ax,
                     color=color)
     plt.title(" Comparison of states vs rspm")               
     st.pyplot(fig)
+    st.info('From the above figure, we concluded that RSPM level is highest in Delhi')
 
 
 elif choice == options[5]:
@@ -117,33 +119,35 @@ elif choice == options[5]:
                     color=color)
     plt.title(" Comparison of states vs spm")
     st.pyplot(fig)
+    st.info('From the above figure, we concluded that SPM level is highest in Uttar Pradesh ')
 
 
 
 elif choice == options[6]:
       st.header("Comparison OF  City vs so2")
       fig,ax = plt.subplots()
-      location_limit = st.sidebar.number_input("How many location",3,25,10)
-      fig,ax = plt.subplots()
-      df[['so2','location']].groupby(["location"]).median().sort_values(by='so2',ascending=False).head(location_limit).plot(
-                    kind='bar', 
-                    figsize=(5,7),
-                    ax=ax,)
-                
-      
-      st.pyplot(fig)
+      color = st.sidebar.color_picker("select graph color")
+      location = st.sidebar.radio("select the state",df.location.unique().tolist())
+
+      try:
+       fig,ax=plt.subplots()
+       df[df['location']==location].set_index('so2')['Estimated level of so2 (%)'].plot(kind='bar',figsize=(5,15),ax=ax,color=color)
+       plt.title(f'so2 in {location}',fontsize="11",color='c')
+       st.pyplot(fig)
+      except Exception as e:
+        st.error(f"Please ask the admin {e}")
+      st.info('From the above figure, we concluded that SO2 level is highest in Dharuhera ')
       
 elif choice== options[7]:
       st.header("Comparison of City no2")
       location_limit = st.sidebar.number_input("How many location",3,25,10)
       fig,ax = plt.subplots()
       df[['no2','location']].groupby(["location"]).median().sort_values(by='no2',ascending=False).head(location_limit).plot(
-                    kind='bar', 
+                    kind='barh', 
                     figsize=(5,7),
                     ax=ax,)
-                
-      
       st.pyplot(fig)
+      st.info('From the above figure, we concluded that NO2 level is highest in Hawroh ')
       
       
     
@@ -154,12 +158,11 @@ elif choice == options[8]:
         location_limit = st.sidebar.number_input("How many location",3,25,10)
         fig,ax = plt.subplots()
         df[['spm','location']].groupby(["location"]).median().sort_values(by='spm',ascending=False).head(location_limit).plot(
-                    kind='bar', 
+                    kind='barh', 
                     figsize=(5,7),
                     ax=ax,)
-                
-      
         st.pyplot(fig)
+        st.info('From the above figure, we concluded that SPM level is highest in Meerut ')
         
 
      
@@ -169,12 +172,11 @@ elif choice== options[9]:
         location_limit = st.sidebar.number_input("How many location",3,25,10)
         fig,ax = plt.subplots()
         df[['rspm','location']].groupby(["location"]).median().sort_values(by='rspm',ascending=False).head(location_limit).plot(
-                    kind='bar', 
+                    kind='barh', 
                     figsize=(5,7),
                     ax=ax,)
-                
-      
         st.pyplot(fig)
+        st.info('From the above figure, we concluded that RSPM level is highest in Gaziabad')
 
 else :
     pass
